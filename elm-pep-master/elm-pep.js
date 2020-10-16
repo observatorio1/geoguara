e3371820a4b7e134003f1ed5af65a8e62582c34d
observatorio1 <71828772+observatorio1@users.noreply.github.com>
2020-10-16 10:07:40 -0300

    Update elm-pep.js

--- elm-pep-master/elm-pep.js
+++ elm-pep-master/elm-pep.js
@@ -1,10 +1,3 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-// Variable to hold current primary touch event identifier.
-// iOS needs this since it does not attribute
-// identifier 0 to primary touch event.
 let primaryTouchId = null;
 
 // Variable to hold mouse pointer captures.
